f82a2232cd0f2ffa7ec94a16c1e07cb344f21f4a
hugo Aparicio <hugo.aparicio8@me.com>
2019-03-14 15:29:07 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+//import RoomList from './components/RoomList';
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBCXOStLJdci2TCZcpRzXoPXC4HKULcJSs",
+    authDomain: "bloc-chat-f2e93.firebaseapp.com",
+    databaseURL: "https://bloc-chat-f2e93.firebaseio.com",
+    projectId: "bloc-chat-f2e93",
+    storageBucket: "bloc-chat-f2e93.appspot.com",
+    messagingSenderId: "40221233886"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+            hohoho
+        
         </header>
       </div>
     );
