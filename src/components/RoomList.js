4aa4ef9dde278da35f52c672b224ca7d788c4fde
hugo Aparicio <hugo.aparicio8@me.com>
2019-04-02 14:55:01 -0700

    Update RoomList.js

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,29 +5,38 @@ class RoomList extends Component{
         super(props);
         this.state = {
             rooms: [],
-            title: []
+            title: ''
         };
         this.roomsRef = this.props.firebase.database().ref('rooms');
-    }
-
-    componentDidMount() {
-        this.roomsRef.on('child_added', snapshot => {
-            const room = snapshot.val();
-            room.key = snapshot.key;
-            this.setState({ rooms: this.state.rooms.concat( room ) })
-
-        });
+        console.log(this.roomsRef);
       }
+
+      componentDidMount() {
+        this.roomsRef.on('child_added', snapshot => {
+          const room = snapshot.val();
+          room.key = snapshot.key;
+          this.setState({rooms: this.state.rooms.concat( room )})
+        }); 
+      }
+        /*componentWillUnmount() {
+            this.roomsRef.off("child_added", (snapshot) => {
+                const room = snapshot.val();
+                room.key= snapshot.key;
+                this.setState({rooms: this.state.rooms.concat( room ) })
+            })
+        }*/
+      
 
 render() {
     return(
-    <div className='roomListDiv'>
-    <ul className='roomList'>
-    {this.state.rooms.map( (room) =>
-    <li className="roomNames" key={room.key}>{room.title}</li>
-    )}
-    </ul>
-    </div>
+        <div className='roomListDiv'>
+            <h2>Room List</h2>
+            <ul className='roomList'>
+                {this.state.rooms.map( (room) =>
+                    <li className="roomNames" key={room.key}>{room.title}</li>
+                )}
+            </ul>
+        </div>
     )}
 }
 
