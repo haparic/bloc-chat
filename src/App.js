fea0f65b49c34fbb2203a5de8052237200c7aeae
hugo Aparicio <hugo.aparicio8@me.com>
2019-04-02 15:10:49 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {Route, Link } from 'react-router-dom';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
