b9c62cbe231363557618592a49c8332070d57fb8
hugo Aparicio <hugo.aparicio8@me.com>
2019-04-02 20:39:06 -0700

    Update RoomList.js

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -8,6 +8,8 @@ class RoomList extends Component{
             name: " "
         };
         this.roomsRef = this.props.firebase.database().ref('rooms');
+        this.createNewRoom = this.createNewRoom.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         console.log(this.roomsRef);
       }
 
@@ -18,6 +20,15 @@ class RoomList extends Component{
           this.setState({rooms: this.state.rooms.concat( room )})
         }); 
       }
+      createNewRoom(e) {
+          e.preventDefault();
+        this.roomsRef.push({ name: this.state.name });
+        this.setState({ name: "" });
+      }
+      handleChange(e) {
+        this.setState({ name: e.target.value });
+      }
+    
         /*componentWillUnmount() {
             this.roomsRef.off("child_added", (snapshot) => {
                 const room = snapshot.val();
@@ -28,6 +39,13 @@ class RoomList extends Component{
       
 
 render() {
+    const newRoomForm = (
+        <form onSubmit={this.createNewRoom}>
+        <input type="text" value={this.state.name} placeholder="Enter new chat name" onChange={this.handleChange} />
+        <input type="submit" value="Create" />
+      </form>
+    );
+    
     return(
         <div className='roomListDiv'>
             <h2>Room List</h2>
@@ -36,7 +54,9 @@ render() {
                     <li className="roomNames" key={room.key}>{room.name}</li>
                 )}
             </ul>
+            <div>{newRoomForm}</div>
         </div>
+    
     )}
 }
 
