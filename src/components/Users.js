b55883ce299f1f095d7903e25c58a51b5b01ca98
hugo Aparicio <hugo.aparicio8@me.com>
2019-04-17 11:13:54 -0700

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react';
 
+
 class User extends Component {
-    constructor(props) {
-        super(props);
-        this.signIn = this.signIn.bind(this);
-        this.signOut = this.signOut.bind(this);
+  constructor(props){
+  super(props);
+
+    this.signIn.bind(this);
+    this.signOut.bind(this);
+
     }
 
-    componentDidMount() {
-        this.props.firebase.auth().onAuthStateChanged( user => {
-          this.props.activeUser(user);
-        });
-      }
-      
-      signIn() {
-        const provider = new this.props.firebase.auth.GoogleAuthProvider();
-        this.props.firebase.auth().signInWithPopup( provider ).then(function(result) {
-          console.log(result);
-          
-      });
-      }
-      
-      signOut() {
-        this.props.firebase.auth().signOut()
-      }
-    
 
-render () {
-  return(
-   <div className="sign in button">
-    <button className="sign in" onClick= {this.signIn}>Log In</button>
-    <button className="sign out" onClick= {this.signOut}>Log out</button> 
-      <p>
-       Welcome, {this.props.user ? this.props.user.displayName : "Guest"}! 
-      </p>
-    </div> 
-  )
-}
+  signIn(){
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signInWithPopup( provider ).then((result) => {
+      console.log(this.props.user);
+    });
+  }
+
+  signOut(){
+    this.props.firebase.auth().signOut().then((result) => {
+      console.log("sign out successful");
+      this.props.setUser(null);
+    });
+  }
+
+  componentDidMount(){
+    this.props.firebase.auth().onAuthStateChanged(user => {
+      this.props.setUser(user);
+    })
+  }
+
+  render () {
+    return (
+
+      <section>
+        <div id="user-name">{this.props.user}</div>
+
+        {this.props.user === 'Guest' ?
+            <button id="sign-in" onClick={() => this.signIn()}>Log In</button>
+          :
+            <button id="sign-out" onClick={() => this.signOut()}>Log Out</button>
+          }
+          {console.log(this.props.user)}
+      </section>
+
+    );
+  }
+
 }
 
 export default User
