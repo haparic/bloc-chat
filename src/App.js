54d9261b8fe31687794458c0126693a3e7f8bc81
hugo Aparicio <hugo.aparicio8@me.com>
2019-04-17 11:13:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
 import User from './components/Users';
 
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBCXOStLJdci2TCZcpRzXoPXC4HKULcJSs",
@@ -16,15 +17,108 @@ import User from './components/Users';
   };
   firebase.initializeApp(config);
 
+  class App extends Component {
+    constructor(props){
+      super(props)
+    
+      this.state = {
+        currentRoom: '',
+        user: null
+      };
+    }
+    
+    setCurrentRoom(room ) {
+      this.setState({currentRoom: room})
+    }
+    
+    setUser(user){
+      if (user === null ) {
+        return this.setState({ user: "Guest"})
+      } else return this.setState({user: user.displayName})
+    }
+    
+      render() {
+    
+        const showMessages = this.state.currentRoom;
+    
+        return (
+          <div className='App'>
+            <header>
+              <h1>Chat:  {this.state.currentRoom.name}</h1>
+              <h3>
+                <User firebase = {firebase} setUser={this.setUser.bind(this)} user={this.state.user} />
+              </h3>
+            </header>
+    
+            <aside>
+              <RoomList firebase={firebase} currentRoom={this.setCurrentRoom.bind(this)}/>
+            </aside>
+    
+            <main>
+    
+              <div id="messagePlane">
+                {showMessages ? (<MessageList firebase={firebase} currentRoom={this.state.currentRoom.key} user={this.state.user} />) : (null) }
+              </div>
+            </main>
+          </div>
+        );
+      }
+    }
+    
+    export default App;
+    
+/*
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeRoom: '',
+        currentUser: null,
+      };
+  
+      this.setActiveRoom = this.setActiveRoom.bind(this);
+      this.setUser = this.setUser.bind(this);
+    }
+  
+    setActiveRoom(room) {
+      this.setState({ activeRoom: room });
+    }
+  
+    setUser(user) {
+      this.setState({ currentUser: user});
+    }
+  
+  
+    render() {
+      return (
+        <div className="App">
+          <h1 className="App-title">BLOC CHAT</h1>
+  
+          <aside id="sidebar">
+            <RoomList firebase={firebase} setActiveRoom={this.setActiveRoom} />
+            { this.state.activeRoom ?
+              (<MessageList firebase={firebase} activeRoom={this.state.activeRoom} currentUser={this.state.currentUser}/>) : (null)
+            }
+          </aside>
+          <h3 className="chatRooms">{this.state.activeRoom.name || "Your Chat Rooms"}</h3>
+          <User firebase={firebase} currentUser={this.state.currentUser} setUser={this.setUser}/>
+        </div>
+      );
+    }
+  }
+  
+  export default App;
+ 
+  
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-    activeRoom: null,
+    activeRoom: "",
     username: null
     };
-
+    this.activeRoom = this.activeRoom.bind(this);
     this.activeUser = this.activeUser.bind(this);
   }
   
@@ -32,7 +126,9 @@ class App extends Component {
     this.setState({activeRoom:room});
   }
   activeUser(user) {
-    this.setState({ username: user});
+    if (user === null ) {
+      return this.setState({ user: "Guest"})
+    } else return this.setState({user: user.displayName})
   }
 
   render() {
@@ -43,14 +139,15 @@ class App extends Component {
           </div>
           <div>
             <User username=
-            {this.state.activeUser} firebase={firebase} setActiveUser={(user)=>this.username(user)}/>
+            {this.state.username} firebase={firebase} activeUser={(user)=>this.username(user)}/>
           </div>
             <main>
               <MessageList activeRoom={this.state.activeRoom} firebase={firebase}/>
             </main>
-      </div>
+      </div> 
     );
-  }
-}
+  } 
+} 
 
-export default App;
+export default App
+*/
