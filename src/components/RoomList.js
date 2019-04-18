9ec9af3558e5e5c926b0ae19a0fbd5202cc6f616
hugo Aparicio <hugo.aparicio8@me.com>
2019-04-17 18:10:54 -0700

    Update RoomList.js

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 
-
 class RoomList extends Component {
   constructor(props) {
     super(props);
-
-
-
     this.state = {
       rooms: [],
       roomName: " ",
@@ -37,7 +33,7 @@ class RoomList extends Component {
     this.setState({roomName: " "});
   }
 
-  selectRoom(key) {
+  activeRoom(key) {
     this.props.currentRoom(key);
   }
 
@@ -51,14 +47,14 @@ class RoomList extends Component {
           <ul>
             {this.state.rooms.map( ( room ) => {
               return (
-                <div key={room.key} onClick={(e)=> this.selectRoom(room, e)}> {room.name}</div>
+                <div key={room.key} onClick={(e)=> this.activeRoom(room, e)}> {room.name}</div>
               )
             })}
           </ul>
         </div>
-
+            <h2>Create New Room</h2>
         <form className='roomInput'>
-          <input type='text' value={this.state.roomName} onChange={(e) => this.handleChange(e)} />
+          <input type='text' value={this.state.roomName} placeholder="Enter New Room" onChange={(e) => this.handleChange(e)} />
           <input type= 'submit' onClick= {(e) => this.createRoom(e)} />
         </form>
 
@@ -68,71 +64,3 @@ class RoomList extends Component {
 }
 
 export default RoomList;
-
-
-/*
-
-import React, { Component } from 'react';
-
-class RoomList extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            rooms: []
-        };
-        this.roomsRef = this.props.firebase.database().ref('rooms');
-        this.createNewRoom = this.createNewRoom.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        console.log(this.roomsRef);
-      }
-
-      componentDidMount() {
-        this.roomsRef.on('child_added', snapshot => {
-          const room = snapshot.val();
-          room.key = snapshot.key;
-          this.setState({rooms: this.state.rooms.concat( room )})
-        }); 
-      }
-      createNewRoom(e) {
-          e.preventDefault();
-        this.roomsRef.push({ name: this.state.name });
-        this.setState({ name: "" });
-      }
-      handleChange(e) {
-        this.setState({ name: e.target.value });
-      }
-      activeRoom(e,name) {
-          console.log('activeRoom')
-          this.props.setActiveRoom(name);
-      }
-      
-    
-      
-
-render() {
-    const newRoomForm = (
-        <form onSubmit={this.createNewRoom}>
-        <input type="text" value={this.state.name} placeholder="Enter new chat name" onChange={this.handleChange} />
-        <input type="submit" value="Create" />
-      </form>
-    );
-
-    return(
-        <section className='roomChange'>
-        <div className='roomListDiv'>
-            <h2>Room List</h2>
-            <ul className='roomList'>
-                {this.state.rooms.map( (room) =>
-                    <li className="roomNames" key={room.key}
-                    onClick={(e) => this.activeRoom(e,room.key)}>{room.name} 
-                    </li>
-                )}
-            </ul>
-            <div>{newRoomForm}</div>
-        </div>
-        </section>
-    
-    )}
-}
-
-export default RoomList */
